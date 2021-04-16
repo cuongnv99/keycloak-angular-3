d40e0351bc9b360f4f9f1d16ab1ecbe4a440336f
cuongnv99 <cuongnv99@viettel.com.vn>
2021-04-16 11:28:48 +0700

    1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  envName: 'local',
+  keycloak: {
+    issuer:'http://10.60.98.105:9080/auth/',
+    realm: 'master',
+    clientId: 'angular',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  envName: 'local',
+  keycloak: {
+    issuer:'http://10.60.98.105:9080/auth/',
+    realm: 'master',
+    clientId: 'angular',
+  }
+};
